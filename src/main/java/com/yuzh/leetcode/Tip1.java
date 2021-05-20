aef02adb7e433046b8cd0403b32c2b3bb1183085
yuzhihui <844370396@qq.com>
2021-05-20 23:22:15 +0800

    Initial commit

---
+++ src/main/java/com/yuzh/leetcode/Tip1.java
@@ -0,0 +1,11 @@
+package com.yuzh.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class Tip1 {
+    @Test
+    public int[] twoSum(int[] nums, int target) {
+        System.out.println("fdsjlfjsljfsjf");
+        return null;
+    }
+}
