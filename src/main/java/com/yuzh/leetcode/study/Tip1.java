fc79aa176a07314ebe0a7bda7d926ee3cbdbab36
yuzhihui <844370396@qq.com>
2021-07-12 17:44:53 +0800

    H 指数

--- src/main/java/com/yuzh/leetcode/study/Tip1.java
+++ src/main/java/com/yuzh/leetcode/study/Tip1.java
@@ -7,28 +7,28 @@ import java.util.HashMap;
 /**
  * 1. 两数之和
  * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
- *
+ * <p>
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
- *
+ * <p>
  * 你可以按任意顺序返回答案。
-
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：nums = [2,7,11,15], target = 9
  * 输出：[0,1]
  * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
  * 示例 2：
- *
+ * <p>
  * 输入：nums = [3,2,4], target = 6
  * 输出：[1,2]
  * 示例 3：
- *
+ * <p>
  * 输入：nums = [3,3], target = 6
  * 输出：[0,1]
- *
- *
+ * <p>
+ * <p>
  * 提示：
- *
+ * <p>
  * 2 <= nums.length <= 103
  * -109 <= nums[i] <= 109
  * -109 <= target <= 109
@@ -37,14 +37,14 @@ import java.util.HashMap;
 @Component
 public class Tip1 {
     public int[] twoSum(int[] nums, int target) {
-        int[] arr=new int[2];
+        int[] arr = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
                 if (i != j) {
                     if (nums[i] + nums[j] == target) {
                         //由于冒泡排序返回相同的结果会执行两次，出现反转的情况，所以，我们会一开将结果反向输入
-                        arr[0]=j;
-                        arr[1]=i;
+                        arr[0] = j;
+                        arr[1] = i;
                         break;
                     }
                 }
@@ -56,16 +56,16 @@ public class Tip1 {
     public int[] twoSum1(int[] nums, int target) {
         int[] indexs = new int[2];
         // 建立k-v ，一一对应的哈希表
-        HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
-        for(int i = 0; i < nums.length; i++){
-            if(hash.containsKey(nums[i])){
+        HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hash.containsKey(nums[i])) {
                 indexs[0] = i;
                 indexs[1] = hash.get(nums[i]);
                 return indexs;
 
             }
             // 将数据存入 key为补数 ，value为下标
-            hash.put(target-nums[i],i);
+            hash.put(target - nums[i], i);
         }
         // // 双重循环 循环极限为(n^2-n)/2
         // for(int i = 0; i < nums.length; i++){
